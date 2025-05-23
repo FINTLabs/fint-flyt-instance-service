d84ec0056a9bc8db83cc62530880db48c4a8e0a6
Egil Ballestad <egil.ballestad@vigoiks.no>
2025-05-23 15:00:56 +0200

    refactor retention time on instance-deleted

--- src/main/java/no/fintlabs/kafka/InstanceDeletedEventProducerService.java
+++ src/main/java/no/fintlabs/kafka/InstanceDeletedEventProducerService.java
@@ -8,6 +8,7 @@ import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
 import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
 import no.fintlabs.kafka.event.topic.EventTopicService;
 import no.fintlabs.model.instance.dtos.InstanceObjectDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -19,13 +20,14 @@ public class InstanceDeletedEventProducerService {
 
     public InstanceDeletedEventProducerService(
             InstanceFlowEventProducerFactory instanceFlowEventProducerFactory,
-            EventTopicService eventTopicService
+            EventTopicService eventTopicService,
+            @Value("${fint.flyt.instance-service.kafka.topic.instance-processing-events-retention-time-ms}") long retentionMs
     ) {
         this.newInstanceEventProducer = instanceFlowEventProducerFactory.createProducer(InstanceObjectDto.class);
         this.topicNameParameters = EventTopicNameParameters.builder()
                 .eventName("instance-deleted")
                 .build();
-        eventTopicService.ensureTopic(topicNameParameters, 0);
+        eventTopicService.ensureTopic(topicNameParameters, retentionMs);
     }
 
     public void publish(InstanceFlowHeaders instanceFlowHeaders) {
