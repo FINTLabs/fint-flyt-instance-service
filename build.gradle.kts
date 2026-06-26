bbc3c06c3349f3dffbf0521b416612cc87ca37e1
Jan Ove Kongshaug <jan.ove.kongshaug@gmail.com>
2026-06-26 12:56:10 +0200

    FFS-2144: Bump dependencies (#40)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("org.springframework.boot") version "3.5.11"
+    id("org.springframework.boot") version "3.5.15"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.github.ben-manes.versions") version "0.53.0"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    kotlin("jvm") version "2.3.10"
-    kotlin("plugin.spring") version "2.3.10"
-    kotlin("plugin.jpa") version "2.3.10"
+    id("com.github.ben-manes.versions") version "0.54.0"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
+    kotlin("jvm") version "2.4.0"
+    kotlin("plugin.spring") version "2.4.0"
+    kotlin("plugin.jpa") version "2.4.0"
 }
 
 group = "no.novari"
@@ -41,14 +41,14 @@ dependencies {
     compileOnly("org.springframework.security:spring-security-config")
     compileOnly("org.springframework.security:spring-security-web")
 
-    implementation("org.springframework.kafka:spring-kafka")
+    // implementation("org.springframework.kafka:spring-kafka")
 
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 
-    implementation("no.novari:flyt-kafka:4.0.0")
-    implementation("no.novari:flyt-web-resource-server:2.0.0")
+    implementation("no.novari:flyt-kafka:7.0.0")
+    implementation("no.novari:flyt-web-resource-server:3.1.0")
 
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
